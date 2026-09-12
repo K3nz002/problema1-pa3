643630cb1e93f11e68f7f8ea65e6bc3df651c243
K3nz002 <gkwfuji@gmail.com>
2026-09-12 16:10:25 -0300

    feat: adição inicial do esqueleto do sistema do problema 1

---
+++ main.py
@@ -0,0 +1,34 @@
+# Bibliotecas
+
+fila_espera = []
+
+def cadastrar(cpf, nome, nascimento):
+    pass
+
+def buscar_cadastro(cpf):
+    pass
+
+def dar_entrada(cpf, risco):
+    if not buscar_cadastro(cpf):
+        print("Paciente não cadastrado.")
+        cadastrar(cpf, nome, nascimento)
+    else:
+        fila_espera.append((f"{cpf} | {risco}\n"))
+
+def chamar_proximo( ):
+    if not fila_espera:
+        return "Fila vazia."
+    
+    if in fila_espera
+
+def tamanho_final():
+    pass
+
+def relatorio_do_dia():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
