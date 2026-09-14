64bc09f47ee89f4e6e5851263198598e032f5219
K3nz002 <gkwfuji@gmail.com>
2026-09-14 11:30:50 -0300

    feat: R3 e R4
    Mudança no tratamento da lista de espera e implementação da chamada por prioridade

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # problema1-pa3
 Triagem no Pronto Atendimento
+
+
+#### Uso de IA
+
+- Pesquisa sobre filas de prioridade e a implementação com heap com o perplexity
--- main.py
+++ main.py
@@ -13,20 +13,51 @@ def dar_entrada(cpf, risco):
         print("Paciente não cadastrado.")
         cadastrar(cpf, nome, nascimento)
     else:
-        fila_espera.append((f"{cpf} | {risco}\n"))
+        if risco.lower() == "emergencia":
+            risco = 1
+        elif risco.lower() == "muito urgente":
+            risco = 2
+        elif risco.lower() == "urgente":
+            risco = 3
+        elif risco.lower() == "pouco urgente":
+            risco = 4
+        elif risco.lower() == "nao urgente":
+            risco = 5
+        fila_espera.append([cpf, risco])
 
 def chamar_proximo( ):
     if not fila_espera:
         return "Fila vazia."
     
-    if in fila_espera
+    for i in range(len(fila_espera)):
+        if fila_espera[i][1] == 1:
+            return "Chamado: " + fila_espera[i][0] + "Risco: Emergência"
+            fila_espera.pop(i)
+            return True
+        elif fila_espera[i][1] == 2:
+            return "Chamado: " + fila_espera[i][0] + "Risco: Muito urgente"
+            fila_espera.pop(i)
+            return True
+        elif fila_espera[i][1] == 3:
+            return "Chamado: " + fila_espera[i][0] + "Risco: Urgente"
+            fila_espera.pop(i)
+            return True
+        elif fila_espera[i][1] == 4:
+            return "Chamado: " + fila_espera[i][0] + "Risco: Pouco urgente"
+            fila_espera.pop(i)
+            return True
+        elif fila_espera[i][1] == 5:
+            return "Chamado: " + fila_espera[i][0] + "Risco: Não urgente"
+            fila_espera.pop(i)
+            return True
+        
 
 def desistir(cpf):
     for i in range(len(fila_espera)):
         if fila_espera[i]["cpf"] == cpf:
             fila_espera.pop(i)
             return True
-    return false
+    return False
 
 def tamanho_final():
     pass
